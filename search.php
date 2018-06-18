372fc5d0dec7f7fdc69468cf565584bb344e3b15
ptibogxiv <support@ptibogxiv.net>
2018-06-18 13:44:28 +0200

    fix  css

--- search.php
+++ search.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <div class="row">
-    
+<DIV class="<?php echo esc_attr(get_theme_mod( 'ptibogxivtheme_container_type')); ?> site-content">
+<DIV class="row">
+<?php dynamic_sidebar('top-widget-area');?>
+ </DIV>
+ <DIV class="row">
     <div class="<?php if(is_active_sidebar('sidebar-right-widget-area')): ?>col-sm-8<?php else: ?>col-sm-12<?php endif; ?>">
       <div id="content" role="main">
         <header>
@@ -13,9 +15,12 @@
       </div><!-- /#content -->
     </div>
     
-    <div class="col-sm-4" id="sidebar" role="navigation">
-       <?php get_sidebar(); ?>
-    </div>
+<?php if(is_active_sidebar('sidebar-right-widget-area') && (get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='right' or get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both')){ ?>    
+    <DIV class="<?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
+    else: ?>col-12 col-md-4<?php endif; ?>" id="rightsidebar" role="navigation">
+<BR><DIV class="card border-light shadow-lg" style="background-color: rgba(256, 256, 256, 0.8)"><DIV class="card-body"><?php dynamic_sidebar('sidebar-right-widget-area'); ?></DIV></DIV><BR>
+    </DIV>
+<?php } ?> 
     
   </div><!-- /.row -->
 </div><!-- /.container -->

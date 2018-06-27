5e4c18a6e94a10cb4e3f94b5dd2538058f0047ea
ptibogxiv <support@ptibogxiv.net>
2018-06-27 15:48:14 +0200

    fix

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
 <?php if(is_active_sidebar('sidebar-right-widget-area') && (get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='right' or get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both')){ ?>    
     <DIV class="<?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
     else: ?>col-12 col-md-4<?php endif; ?>" id="sidebar" role="navigation">
-<BR><DIV class="card border-light shadow-lg" style="background-color: rgba(256, 256, 256, 0.8)"><DIV class="card-body"><?php echo $sidebar_pos; get_sidebar(); ?></DIV></DIV><BR>
+<BR><DIV class="card border-light shadow-lg" style="background-color: rgba(256, 256, 256, 0.8)"><DIV class="card-body"><?php dynamic_sidebar('sidebar-right-widget-area'); ?></DIV></DIV><BR>
     </DIV>
 <?php } ?>   
   </DIV><!-- /.row -->

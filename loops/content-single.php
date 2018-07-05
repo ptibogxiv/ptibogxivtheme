bbd8f5d875d5bffe0aef92e69dbe768756a35a5b
ptibogxiv <support@ptibogxiv.net>
2018-07-05 12:41:56 +0200

    fix shadow

--- loops/content-single.php
+++ loops/content-single.php
@@ -6,7 +6,7 @@ The Single Posts Loop
 ?>
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
 <ARTICLE role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-<DIV class="card border-light shadow-lg" style="background-color: rgba(256, 256, 256, 0.8)">
+<DIV class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="background-color: rgba(256, 256, 256, 0.8)">
 <?php if ( has_post_thumbnail() ){ ?><CENTER><IMG class="card-img-top" src="<?php echo wp_get_attachment_image_url(get_post_thumbnail_id( $post ), 'ptibogxiv_large' ); ?>" alt="<?php the_title()?>"></CENTER><?php } ?>
 <DIV class="card-body">
 <HEADER><TABLE width="100%"><TR><TD><H1><?php the_title()?> <?php edit_post_link('<I class="fas fa-edit"></I>', '<SPAN class="edit-link">', '</SPAN>' ); ?></H1>

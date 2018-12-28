f78d546f4628cae0a2b154ea8036549a2419b106
ptibogxiv <support@ptibogxiv.net>
2018-12-29 00:34:59 +0100

    2.5
    fix compatibility with bootstrap 4.2.1

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -10,29 +10,23 @@ $version='4.2.1';
 } else {
 $type='bootswatch';
 $css=get_theme_mod( 'ptibogxivtheme_css');
-$version='4.1.3';  
+$version='4.2.1';  
 }
  
-	wp_register_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/'.$type.'/'.$version.'/'.$css.'/bootstrap.min.css', false, $version, null);
-	wp_enqueue_style('bootstrap-css');
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/theme/css/'.$type.'/'.$css.'/bootstrap.min.css', array(), $version);
+
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/theme/js/scripts.min.js', array('jquery'), ' ', true );
+
+	wp_enqueue_script( 'font-awesome', '//use.fontawesome.com/releases/v5.6.3/js/all.js', array(), '5.6.3' );
    
-  wp_register_script('jquery-slim-min', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', false, '3.3.1', false);
-	wp_enqueue_script('jquery-slim-min');
-   
-  wp_register_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/'.$version.'/js/bootstrap.min.js', false, $version, false);
-	wp_enqueue_script('bootstrap-js'); 
-  
-  wp_register_style('font-awesome', 'https://use.fontawesome.com/releases/v5.6.3/js/all.jss', false, '5.6.3', false);
-	wp_enqueue_style('font-awesome'); 
-  
   wp_register_style('ptibogxivtheme-css', get_template_directory_uri() . '/theme/css/ptibogxivtheme.css', false, null);
 	wp_enqueue_style('ptibogxivtheme-css');
 
-	wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/js/ptibogxivtheme.js', false, null, true);
-	wp_enqueue_script('ptibogxivtheme-js');
+	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/js/ptibogxivtheme.js', false, null, true);
+	//wp_enqueue_script('ptibogxivtheme-js');
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
 }
-add_action('wp_enqueue_scripts', 'ptibogxivtheme_enqueues', 100);
+add_action('wp_enqueue_scripts', 'ptibogxivtheme_enqueues', 10);

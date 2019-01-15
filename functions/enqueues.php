f57e8bef0757ebeaed2a1ada8798e887655fe7e9
ptibogxiv <support@ptibogxiv.net>
2019-01-15 19:32:49 +0100

    update enqueue

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -19,8 +19,7 @@ $version='4.2.1';
 
 	wp_enqueue_script( 'font-awesome', '//use.fontawesome.com/releases/v5.6.3/js/all.js', array(), '5.6.3' );
    
-  wp_register_style('ptibogxivtheme-css', get_template_directory_uri() . '/theme/css/ptibogxivtheme.css', false, null);
-	wp_enqueue_style('ptibogxivtheme-css');
+	wp_enqueue_style( 'ptibogxivtheme-css', get_template_directory_uri() . '/theme/css/ptibogxivtheme.css', false, $version);
 
 	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/js/ptibogxivtheme.js', false, null, true);
 	//wp_enqueue_script('ptibogxivtheme-js');

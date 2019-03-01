88447e8c8884e27cd8a7baf61aed795140711776
ptibogxiv <support@ptibogxiv.net>
2019-03-01 15:04:03 +0100

    add comment on page

--- loops/content-full.php
+++ loops/content-full.php
@@ -10,9 +10,11 @@ The Page Loop
 <?php if ( has_post_thumbnail() ){ ?><CENTER><IMG class="d-block w-100 img-fluid rounded" src="<?php the_post_thumbnail_url(); ?>" alt="Third slide"></CENTER><BR><?php } ?> 
 <DIV class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="background-color: rgba(256, 256, 256, 0.8)"><DIV class="card-body">
 <HEADER><H1><?php the_title()?> <?php edit_post_link('<I class="fas fa-edit"></I>', '<span class="edit-link">', '</SPAN>' ); ?></H1></HEADER> 
-    <?php the_content()?>
-    <?php wp_link_pages(); ?>
-</DIV></DIV></ARTICLE>
+<?php the_content()?>
+<?php wp_link_pages(); ?>
+</DIV></DIV>
+<?php comments_template(); ?>
+</ARTICLE>
 <?php endwhile; else: ?>
 <?php wp_redirect(get_bloginfo('url').'/404', 404); ?>
 <?php exit; ?>
--- loops/content-page.php
+++ loops/content-page.php
@@ -9,8 +9,10 @@ The Page Loop
 <ARTICLE role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
 <DIV class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="background-color: rgba(256, 256, 256, 0.8)"><DIV class="card-body">
 <HEADER><H1><?php the_title()?> <?php edit_post_link('<I class="fas fa-edit fa-fw"></I>', '<span class="edit-link">', '</SPAN>' ); ?></H1></HEADER>
-    <?php the_content()?>
-    <?php wp_link_pages(); ?></DIV></DIV></ARTICLE>
+<?php the_content()?>
+<?php wp_link_pages(); ?></DIV></DIV>
+<?php comments_template(); ?>
+</ARTICLE>
 <?php endwhile; else: ?>
 <?php wp_redirect(get_bloginfo('url').'/404', 404); ?>
 <?php exit; ?>

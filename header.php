9c79a77ffc7289478285a41eedb42203ee4dd0cf
ptibogxiv <support@ptibogxiv.net>
2019-06-14 13:20:23 +0200

    Update header.php

--- header.php
+++ header.php
@@ -82,7 +82,7 @@ the_custom_logo(); ?>
 if ( function_exists('pll_the_languages') ) {       
 ?><a href="#" data-toggle="modal" data-target="#SelectLang" data-dismiss="modal" title="<?php _e('Choose language', 'ptibogxivtheme'); ?>"><i class='fas fa-language fa-fw fa-2x'></i></a>&nbsp;<?php
 }
-if ( function_exists('doliconnecturl') ) { 
+if ( function_exists('doliconnectid') && doliconnectid('doliaccount') > 0 ) { 
 if ( get_site_option('doliconnect_mode') == 'one' && is_multisite() ) {
 switch_to_blog(1);
 }  

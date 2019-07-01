f77ce6e63bb1e4ddef32da4a41ce1d459483188f
ptibogxiv <support@ptibogxiv.net>
2019-07-01 19:39:04 +0200

    3.3.2

--- functions/widgets.php
+++ functions/widgets.php
@@ -33,8 +33,18 @@ function ptibogxivtheme_widgets_init() {
    */
   register_sidebar( array(
     'name'            => __( 'Footer', 'ptibogxivtheme' ),
-    'id'              => 'footer-widget-area',
-    'description'     => __( 'The footer widget area', 'ptibogxivtheme' ),
+    'id'              => 'payment-footer-widget-area',
+    'description'     => __( 'The payment footer widget area', 'ptibogxivtheme' ),
+    'before_widget'   => '<div class="%1$s %2$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+    
+  register_sidebar( array(
+    'name'            => __( 'Footer', 'ptibogxivtheme' ),
+    'id'              => 'address-footer-widget-area',
+    'description'     => __( 'The address footer widget area', 'ptibogxivtheme' ),
     'before_widget'   => '<div class="%1$s %2$s">',
     'after_widget'    => '</div>',
     'before_title'    => '<h4>',

30aeb020cf616a5b9089190d5e62f97f9d41f55a
ptibogxiv <support@ptibogxiv.net>
2020-04-21 12:13:07 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -23,8 +23,8 @@
       <div id="content" role="main">
         <br>
         <div class="alert alert-warning">
-          <h1><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'b4st'); ?> 404</h1>
-          <p><?php _e('The page you were looking for does not exist.', 'b4st'); ?></p>
+          <h1><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'ptibogxivtheme'); ?> 404</h1>
+          <p><?php _e('The page you were looking for does not exist.', 'ptibogxivtheme'); ?></p>
         </div>
       </div><!-- /#content -->
     </div>

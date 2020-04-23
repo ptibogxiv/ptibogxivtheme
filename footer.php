4ccab66d3ccf83dde38ff72845f0dcfa5ce3a135
ptibogxiv <support@ptibogxiv.net>
2020-04-23 17:10:22 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -112,8 +112,14 @@ $company = callDoliApi("GET", "/setup/company", null, dolidelay('constante', esc
 <?php if (!empty($company->tva_assuj)) {?><br>TVA: <?php echo $company->tva_intra; ?><?php }?>
 <?php if (!empty($company->note_private)) {?><br><?php echo $company->note_private; ?><?php }?></p>
 <p><strong><?php _e('Responsible for publishing', 'ptibogxivtheme'); ?></strong><br><?php echo $company->managers; ?></p>
-<p><strong><?php _e('Hosting', 'ptibogxivtheme'); ?></strong><br>ptibogxiv.net<br>1 rue de la grande brasserie, 59000 LILLE<br>www.ptibogxiv.net<br>SIRET: 83802482600011 - APE6201Z</p>
-</div></div></div></div>
-<?php } ?> 
+<?php if ( defined('PTIBOGXIV_NET') ) { ?>
+<p><strong><?php _e('Hosting', 'ptibogxivtheme'); ?></strong><br>Infomaniak Network SA<br>
+Rue Eugène-Marziano, 25<br>
+CH - 1227 Genève - Suisse<br>
+N° TVA: CHE - 103.167.648<br>
+N° de société: CH - 660 - 0059996 - 1<br>
+Site Internet: <a href="https://www.infomaniak.com/goto/fr/home?utm_term=5de6793fdf41b">Infomaniak</a></p>
+<?php } ?>
+</div></div></div></div><?php } ?>
 </body>
 </html>

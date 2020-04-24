15da21d3cd6e8ac81a82662b86fdc1899e6d26d6
ptibogxiv <support@ptibogxiv.net>
2020-04-24 13:41:07 +0200

    Update single.php

--- single.php
+++ single.php
@@ -9,24 +9,28 @@
  </div>
  <div class="row" <?php if(get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?>style="<?php echo ptibogxivtheme_gradient(); ?>"<?php endif; ?>>
 <?php if(is_active_sidebar('sidebar-left-widget-area') && (get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='left' or get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both')){ ?>    
-    <div class="<?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
-    else: ?>col-12 col-md-4<?php endif; ?>" id="leftsidebar" role="navigation">
-<br><div class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="<?php echo ptibogxivtheme_gradient(); ?>"><div class="card-body"><?php dynamic_sidebar('sidebar-left-widget-area'); ?></div></div><br>
-    </div>
+    <div class="order-2 order-md-1 <?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
+    else: ?>col-12 col-md-4<?php endif; ?>" id="leftsidebar" role="navigation"><br>
+<?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?><div class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="<?php echo ptibogxivtheme_gradient(); ?>"><div class="card-body"><?php endif; ?>
+<?php dynamic_sidebar('sidebar-left-widget-area'); ?>
+<?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?></div></div><?php endif; ?>
+<br></div>
 <?php } ?>  
-    <div class="<?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='none'): ?>col-12<?php
+    <div class="order-1 order-md-2 <?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='none'): ?>col-12<?php
     elseif(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-6<?php
     elseif(is_active_sidebar('sidebar-left-widget-area') OR is_active_sidebar('sidebar-right-widget-area')): ?>col-12 col-md-8<?php 
     else: ?>col-12<?php endif; ?>">
       <div id="content" role="main">
         <?php get_template_part('loops/content', 'single'); ?>
       </div><!-- /#content -->
-    </div>  
-<?php if(is_active_sidebar('sidebar-right-widget-area') && (get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='right' or get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both')){ ?>    
-    <div class="<?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
-    else: ?>col-12 col-md-4<?php endif; ?>" id="rightsidebar" role="navigation">
-<br><div class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="<?php echo ptibogxivtheme_gradient(); ?>"><div class="card-body"><?php dynamic_sidebar('sidebar-right-widget-area'); ?></div></div><br>
     </div>
+<?php if(is_active_sidebar('sidebar-right-widget-area') && (get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='right' or get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both')){ ?>    
+    <div class="order-3 <?php if(get_theme_mod( 'ptibogxivtheme_sidebar_position' )=='both'): ?>col-12 col-md-3<?php
+    else: ?>col-12 col-md-4<?php endif; ?>" id="rightsidebar" role="navigation"><br>
+<?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?><div class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="<?php echo ptibogxivtheme_gradient(); ?>"><div class="card-body"><?php endif; ?>
+<?php dynamic_sidebar('sidebar-right-widget-area'); ?>
+<?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?></div></div><?php endif; ?>
+<br></div>
 <?php } ?>   
   </div><!-- /.row -->
 </div><!-- /.container -->

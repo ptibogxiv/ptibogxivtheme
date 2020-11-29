6b3c58fc45b91308f2c0bf5d41d61fee638ff495
ptibogxiv <support@ptibogxiv.net>
2020-11-29 20:32:09 +0100

    Update search-widget.php

--- functions/search-widget.php
+++ functions/search-widget.php
@@ -2,8 +2,7 @@
 
 function ptibogxivtheme_search_form( $form ) {
 $form = '<form role="search" method="get" id="searchform" action="' . home_url('/') . '" ><div class="input-group"><input type="text" class="form-control" name="s" id="s" placeholder="' . esc_attr__('Your search', 'ptibogxivtheme') . '" aria-label="Search for..." aria-describedby="search-widget">
-<div class="input-group-append"><button class="btn btn-primary" type="submit" id="searchsubmit" ><i class="fas fa-search"></i></button></div>
-</div></form>';
+<button class="btn btn-primary" type="submit" id="searchsubmit" ><i class="fas fa-search"></i></button></div></form>';
 return $form;
 }
 add_filter( 'get_search_form', 'ptibogxivtheme_search_form' );

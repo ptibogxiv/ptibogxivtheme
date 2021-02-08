5f4c744ab6ef502199c65f4042e1ac766f1956f6
ptibogxiv <support@ptibogxiv.net>
2021-02-08 19:03:27 +0100

    wip

--- functions/navbar.php
+++ functions/navbar.php
@@ -38,7 +38,7 @@ class ptibogxivtheme_walker_nav_menu extends Walker_Nav_menu {
 		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
 		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr($item->url) . '"' : '';
 
-		$attributes .= ( $args->walker->has_children ) ? ' class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
+		$attributes .= ( $args->walker->has_children ) ? ' class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
 
 		$item_output = $args->before;
 		$item_output .= ( $depth > 0 ) ? '<a class="dropdown-item"' . $attributes . '>' : '<a' . $attributes . '>';

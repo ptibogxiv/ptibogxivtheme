b9e5fecaa782fb41dcd499cca428304ecfb676db
ptibogxiv <support@ptibogxiv.net>
2023-05-31 21:54:33 +0200

    wip

--- loops/content-full.php
+++ loops/content-full.php
@@ -10,7 +10,7 @@ The Page Loop
 <?php if ( has_post_thumbnail() ){ ?><div class="card mb-3"><img src="<?php echo wp_get_attachment_image_url(get_post_thumbnail_id( $post ), 'ptibogxiv_large' ); ?>" class="card-img-top" alt="Third slide"></div><br><?php } ?> 
 <?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?><div class="card border-light <?php if(!get_theme_mod( 'ptibogxivtheme_shadowcontent' )): ?>shadow-lg<?php endif; ?>" style="<?php echo ptibogxivtheme_gradient(); ?>"><div class="card-body"><?php endif; ?>
 <header><h1><?php the_title()?> <?php if ( ( empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && current_user_can( 'edit_posts' )) || ( empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && ( wp_get_current_user()->show_admin_bar_front != true)) ) { edit_post_link('<i class="fas fa-edit"></i>', '<span class="edit-link">', '</span>' ); } ?></h1></header> 
-<?php the_content()?>
+<?php the_content(); ?>
 <?php wp_link_pages(); ?>
 <?php if(!get_theme_mod( 'ptibogxivtheme_cardcontent' )): ?></div></div><?php endif; ?>
 </article>

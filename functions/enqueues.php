6fdad71aa9a2ff4c3546b99f283935b6c880d6e8
ptibogxiv <support@ptibogxiv.net>
2023-07-26 22:58:13 +0200

    update font awesome

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -22,7 +22,7 @@ $version=$versionbase;
 	wp_enqueue_style( 'bootstrap.min');
 	wp_register_script( 'bootstrap.bundle.min', get_template_directory_uri() . '/theme/js/bootstrap.bundle.min.js', array('jquery'), $version, true);
   	wp_enqueue_script( 'bootstrap.bundle.min');
-  	wp_register_script( 'font-awesome', 'https://use.fontawesome.com/releases/v6.2.1/js/all.js', array(), '6.2.1');
+  	wp_register_script( 'font-awesome', 'https://use.fontawesome.com/releases/v6.4.0/js/all.js', array(), '6.4.0');
 	wp_enqueue_script( 'font-awesome');
 	wp_enqueue_script( 'jquery-masonry', array( 'jquery' ) );
 	wp_register_style( 'ptibogxivtheme', get_template_directory_uri() . '/theme/css/ptibogxivtheme.css', false, $version);

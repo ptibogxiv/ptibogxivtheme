4adb4265cab45f00a01b2d0aa527119a14eb04bc
ptibogxiv <support@ptibogxiv.net>
2023-07-27 11:05:21 +0200

    upgrade boostrap

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -4,8 +4,8 @@ function ptibogxivtheme_enqueues() {
 
 /* Styles */
 if ( empty(get_theme_mod( 'ptibogxivtheme_css')) || get_theme_mod( 'ptibogxivtheme_css') == 'css' ) {
-$css='css';
-$versionbase = '5.3.0'; 
+$css='boostrap/css';
+$versionbase = '5.3.1'; 
 $version=$versionbase; 
 } else {
 $css='bootswatch/'.get_theme_mod( 'ptibogxivtheme_css');
@@ -14,20 +14,20 @@ $versionbase=$version;
 }
 
 if (!empty(get_theme_mod( 'ptibogxivtheme_css')) && $version != $versionbase && empty(get_option('doliconnectbeta'))) {
-$css='css';
+$css='boostrap/css';
 $version=$versionbase;
 }
 
 	wp_register_style( 'bootstrap.min', get_template_directory_uri() . '/theme/' .$css.'/bootstrap.min.css', array(), $version );
 	wp_enqueue_style( 'bootstrap.min');
-	wp_register_script( 'bootstrap.bundle.min', get_template_directory_uri() . '/theme/js/bootstrap.bundle.min.js', array('jquery'), $version, true);
+	wp_register_script( 'bootstrap.bundle.min', get_template_directory_uri() . '/theme/boostrap/js/bootstrap.bundle.min.js', array('jquery'), $version, true);
   	wp_enqueue_script( 'bootstrap.bundle.min');
   	wp_register_script( 'font-awesome', 'https://use.fontawesome.com/releases/v6.4.0/js/all.js', array(), '6.4.0');
 	wp_enqueue_script( 'font-awesome');
 	wp_enqueue_script( 'jquery-masonry', array( 'jquery' ) );
-	wp_register_style( 'ptibogxivtheme', get_template_directory_uri() . '/theme/css/ptibogxivtheme.css', false, $version);
+	wp_register_style( 'ptibogxivtheme', get_template_directory_uri() . '/theme/custom/css/ptibogxivtheme.css', false, $version);
 	wp_enqueue_style( 'ptibogxivtheme');
-	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/js/ptibogxivtheme.js', false, null, true);
+	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/custom/js/ptibogxivtheme.js', false, null, true);
 	//wp_enqueue_script('ptibogxivtheme-js');
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');

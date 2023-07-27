ea7f86c0b276c151b5dfff3212efc6ce84332988
ptibogxiv <support@ptibogxiv.net>
2023-07-27 11:08:50 +0200

    fix

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -4,30 +4,30 @@ function ptibogxivtheme_enqueues() {
 
 /* Styles */
 if ( empty(get_theme_mod( 'ptibogxivtheme_css')) || get_theme_mod( 'ptibogxivtheme_css') == 'css' ) {
-$css='boostrap/css';
+$css = 'bootstrap/css';
 $versionbase = '5.3.1'; 
-$version=$versionbase; 
+$version = $versionbase; 
 } else {
-$css='bootswatch/'.get_theme_mod( 'ptibogxivtheme_css');
-$version='5.3.0'; 
-$versionbase=$version; 
+$css = 'bootswatch/'.get_theme_mod( 'ptibogxivtheme_css');
+$version = '5.3.0'; 
+$versionbase = $version; 
 }
 
 if (!empty(get_theme_mod( 'ptibogxivtheme_css')) && $version != $versionbase && empty(get_option('doliconnectbeta'))) {
-$css='boostrap/css';
-$version=$versionbase;
+$css = 'bootstrap/css';
+$version = $versionbase;
 }
 
-	wp_register_style( 'bootstrap.min', get_template_directory_uri() . '/theme/' .$css.'/bootstrap.min.css', array(), $version );
+	wp_register_style( 'bootstrap.min', get_template_directory_uri().'/theme'.$css.'/bootstrap.min.css', array(), $version );
 	wp_enqueue_style( 'bootstrap.min');
-	wp_register_script( 'bootstrap.bundle.min', get_template_directory_uri() . '/theme/boostrap/js/bootstrap.bundle.min.js', array('jquery'), $version, true);
+	wp_register_script( 'bootstrap.bundle.min', get_template_directory_uri().'/theme/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), $version, true);
   	wp_enqueue_script( 'bootstrap.bundle.min');
   	wp_register_script( 'font-awesome', 'https://use.fontawesome.com/releases/v6.4.0/js/all.js', array(), '6.4.0');
 	wp_enqueue_script( 'font-awesome');
 	wp_enqueue_script( 'jquery-masonry', array( 'jquery' ) );
-	wp_register_style( 'ptibogxivtheme', get_template_directory_uri() . '/theme/custom/css/ptibogxivtheme.css', false, $version);
+	wp_register_style( 'ptibogxivtheme', get_template_directory_uri().'/theme/custom/css/ptibogxivtheme.css', false, $version);
 	wp_enqueue_style( 'ptibogxivtheme');
-	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri() . '/theme/custom/js/ptibogxivtheme.js', false, null, true);
+	//wp_register_script('ptibogxivtheme-js', get_template_directory_uri().'/theme/custom/js/ptibogxivtheme.js', false, null, true);
 	//wp_enqueue_script('ptibogxivtheme-js');
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');

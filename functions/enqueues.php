89a9c4e8a3dc111a860ed1f1afacaef0ff8f0ee4
ptibogxiv <support@ptibogxiv.net>
2023-09-17 19:54:43 +0200

    bootstrap 5.3.2

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -3,7 +3,7 @@
 function ptibogxivtheme_enqueues() {
 	if ( empty(get_theme_mod( 'ptibogxivtheme_css')) || get_theme_mod( 'ptibogxivtheme_css') == 'css' ) {
 		$css = 'bootstrap/css';
-		$versionbase = '5.3.1'; 
+		$versionbase = '5.3.2'; 
 		$version = $versionbase; 
 	} else {
 		$css = 'bootswatch/'.get_theme_mod( 'ptibogxivtheme_css');

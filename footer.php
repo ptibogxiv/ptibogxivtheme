bd27a0b17aa0f8cba5e9a11d5b6b93d6e0024560
ptibogxiv <support@ptibogxiv.net>
2026-08-02 15:24:07 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -114,14 +114,14 @@ echo "</script>";
           ) );
         ?>
       </div>
-      <?php if ( ( empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && current_user_can( 'edit_posts' )) || ( !empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && current_user_can( 'edit_posts') && wp_get_current_user()->show_admin_bar_front == 'false' ) ) { ?>
-        <div class="offcanvas-footer m-3">
-          <p class="text-center"><small><i class="fa-solid fa-copyright"></i> <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?><br><?php _e('All rights reserved', 'ptibogxivtheme'); ?><?php if (function_exists('doliModalButton')) { ?> - <?php echo doliModalButton('legacy', 'legacyfooter', __('Legal notice', 'ptibogxivtheme'), 'a' , 'text-reset'); } ?></p>
+      <div class="offcanvas-footer m-3">
+        <p class="text-center"><small><i class="fa-solid fa-copyright"></i> <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?><br><?php _e('All rights reserved', 'ptibogxivtheme'); ?><?php if (function_exists('doliModalButton')) { ?> - <?php echo doliModalButton('legacy', 'legacyfooter', __('Legal notice', 'ptibogxivtheme'), 'a' , 'text-reset'); } ?></p>    
+        <?php if ( ( empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && current_user_can( 'edit_posts' )) || ( !empty(get_theme_mod( 'ptibogxivtheme_adminbar')) && current_user_can( 'edit_posts') && wp_get_current_user()->show_admin_bar_front == 'false' ) ) { ?>
           <div class="d-grid gap-2"> 
             <a type="button" class="btn btn-outline-secondary btn-lg" href="<?php echo admin_url('index.php'); ?>" title="<?php _e('Administration', 'ptibogxivtheme'); ?>""><i class="fa-solid fa-cogs fa-fw"></i> <?php _e('Administration', 'ptibogxivtheme'); ?></a>
           </div>
-        </div>
-      <?php } ?>
+        <?php } ?>
+      </div>
     </div>
   </div>
 </nav>
